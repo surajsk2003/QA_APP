9a1a81a2179f3caf17c5cfcff850bdbc201880ba
Suraj kumar <surajkumarsksk2000@gmail.com>
2025-01-18 08:30:13 +0530

    added an delete file in question

--- questions/views.py
+++ questions/views.py
@@ -28,3 +28,22 @@ def createRoom(request):
 
     context = {'form': form}
     return render(request, 'questions/room_form.html', context)
+
+def updateRoom(request, pk):
+    room  = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Redirect to home page after updating the room
+
+    context = {'form': form}
+    return render(request, 'questions/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'questions/delete.html',  {'obj':room} )
